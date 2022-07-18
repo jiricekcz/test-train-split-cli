0646b6fb1c4aa22ae320a1808f7d290217ff4efd
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2022-07-18 11:54:41 +0200

    pairwise alignment calculator

--- src/__main__.py
+++ src/__main__.py
@@ -1,10 +1,8 @@
 from pre_run import pre_cli_only, pre_use
 from cli import CLI
 
-
 pre_use()
 if __name__ == "__main__":
     pre_cli_only()
     cli = CLI()
     cli.run()
-
---
+++ src/logic/pairwise_alignment_distance_calculator.py
@@ -0,0 +1,26 @@
+from typing import Callable
+from common.interfaces.assest_manager import IAssetManager, IAsset
+from common.interfaces.distance_calculator import IDistanceCalculator
+from common.interfaces.distance_matrix import IDistanceMatrix
+from Bio.Align import PairwiseAligner
+
+
+class PaiwiseAlignmentDistanceCalculator(IDistanceCalculator):
+    aligner: PairwiseAligner
+    def __init__(self):
+        self.aligner: PairwiseAligner = PairwiseAligner()
+
+    def calculateDistances(
+            self,
+            distances: IDistanceMatrix,
+            assets: IAssetManager,
+            statusReport: Callable[[int, int, float], None] = ...) -> None:
+        total = distances.getMatrixSize()**2
+        for t, x, y, set in distances.elements():
+            set(self.calculateDistance(assets.getAsset(x), assets.getAsset(y)))
+            statusReport(t, total, float(t) / float(total))
+
+    def calculateDistance(self, asset1: IAsset, asset2: IAsset) -> float:
+        score = self.aligner.score(asset1.sequence, asset2.sequence)
+        min_size = min(len(asset1.sequence), len(asset2.sequence))
+        return score / min_size
