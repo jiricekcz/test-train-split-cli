7923468fe33049eee0e3eefec43db1d5a09fa9c8
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2022-09-01 13:56:26 +0200

    fix of alignment distance calculator to use alignment and not score

--- src/cli.py
+++ src/cli.py
@@ -6,7 +6,7 @@ from common.types.cli import Args
 
 from logic.asset_manager import MemoryAssetManager
 from logic.fasta_asset_loader import FastaAssetLoader
-from logic.pairwise_alignment_distance_calculator import PairwiseAlignmentDistanceCalculator
+from logic.pairwise_alignment_distance_calculator import PairwiseAlignmentDistanceCalculator, PairwiseAlignmentScoreDistanceCalculator
 from logic.distance_matrix import NumpyDistanceMatrixDiskBackup
 from logic.agglomerative_clustering_splitter import AgglomerativeClusteringSplitter
 
@@ -20,14 +20,14 @@ class CLI:
 
     def run(self) -> None:
         self.parseArguments()
-        limit = None
+        limit = 200
         assets = MemoryAssetManager()
         loader = FastaAssetLoader(path(__file__, "../data/holo4k.fasta"))
         loader.loadAssets(assets, limit=limit)
         filenameAddition = "" if limit == None else f"_{limit}x{limit}"
         assetCount = assets.getAssetCount()
-        manager = SplitManager(NumpyDistanceMatrixDiskBackup(assetCount, path(__file__, f"../data/out/matrix_BLOSUM62{filenameAddition}.npy"), save_loop_length=100_000), assets,
-                               PairwiseAlignmentDistanceCalculator(), AgglomerativeClusteringSplitter(headstartFactor=15))
+        manager = SplitManager(NumpyDistanceMatrixDiskBackup(assetCount, path(__file__, f"../data/out/matrix_BLOSUM62_SCORE{filenameAddition}.npy"), save_loop_length=100_000), assets,
+                               PairwiseAlignmentScoreDistanceCalculator(), AgglomerativeClusteringSplitter(headstartFactor=15))
 
         t0 = time.time()
         manager.calculateDistances()
--- src/logic/pairwise_alignment_distance_calculator.py
+++ src/logic/pairwise_alignment_distance_calculator.py
@@ -3,7 +3,7 @@ from common.interfaces.asset_manager import IAssetManager, IAsset
 from common.interfaces.distance_calculator import IDistanceCalculator
 from common.interfaces.distance_matrix import IDistanceMatrix
 from Bio.Align import PairwiseAligner, substitution_matrices
-
+import numpy as np
 class PairwiseAlignmentDistanceCalculator(IDistanceCalculator):
     aligner: PairwiseAligner
 
@@ -22,6 +22,33 @@ class PairwiseAlignmentDistanceCalculator(IDistanceCalculator):
             statusReport(t, total, float(t) / float(total))
 
     def calculateDistance(self, asset1: IAsset, asset2: IAsset) -> float:
-        score = self.aligner.score(asset1.sequence, asset2.sequence)
+        alignments = self.aligner.align(asset1.sequence, asset2.sequence)
         min_size = min(len(asset1.sequence), len(asset2.sequence))
-        return score / min_size
+        distance = np.sum(np.diagonal(alignments[0].substitutions)) / min_size
+        return distance
+
+
+# Does not work because scores seem to not work in negative. Negative scores are substituted with 0.
+# class PairwiseAlignmentScoreDistanceCalculator(IDistanceCalculator):
+#     aligner: PairwiseAligner
+
+#     def __init__(self):
+#         self.aligner: PairwiseAligner = PairwiseAligner()
+#         self.aligner.substitution_matrix = substitution_matrices.load("BLOSUM62")
+
+#     def calculateDistances(
+#             self,
+#             distances: IDistanceMatrix,
+#             assets: IAssetManager,
+#             statusReport: Callable[[int, int, float], None] = ...) -> None:
+#         total = distances.getMatrixSize()**2
+#         for t, x, y, set in distances.elementsToFill():
+#             set(self.calculateDistance(assets.getAsset(x), assets.getAsset(y)))
+#             statusReport(t, total, float(t) / float(total))
+
+#     def calculateDistance(self, asset1: IAsset, asset2: IAsset) -> float:
+#         score = self.aligner.score(asset1.sequence, asset2.sequence)
+#         min_size = min(len(asset1.sequence), len(asset2.sequence))
+#         print(self.aligner.score("ME", "LC"))
+#         return score / min_size
+        
