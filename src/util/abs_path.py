c337b5cbbfdf040bad35d4fe234b22fdf6068fed
jiricekcz <36630605+jiricekcz@users.noreply.github.com>
2022-09-01 22:00:24 +0200

    comments

--- src/util/abs_path.py
+++ src/util/abs_path.py
@@ -1,4 +1,9 @@
 import os
+
+
 def path(__file, file) -> str:
+    """
+    Used to calculate the absolute path of a file based on the path to the calle. This is used to make the execution predictable regardless of where the program is run from and its current working directory.
+    """
     dir_path = os.path.dirname(os.path.realpath(__file))
     return os.path.join(dir_path, file)
